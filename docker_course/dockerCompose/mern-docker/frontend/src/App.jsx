b07f867584fb3519adfae123d232bed3e365b912
Michael Javier <m.mota@concentra.com.do>
2024-03-24 12:08:32 -0400

    usando compose watch

--- docker_course/dockerCompose/mern-docker/frontend/src/App.jsx
+++ docker_course/dockerCompose/mern-docker/frontend/src/App.jsx
@@ -1,30 +1,33 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Create from "./pages/Create";
+import Home from './pages/Home';
+import Create from './pages/Create';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <nav className="navbar">
-        <Link to="/" className="navbar_title">
+      <nav className='navbar'>
+        <Link to='/' className='navbar_title'>
           <h1>Anime</h1>
         </Link>
-        <div className="navbar_links">
-          <Link to="/" className="navbar_link">
+        <div className='navbar_links'>
+          <Link to='/' className='navbar_link'>
             Home
           </Link>
-          <Link to="/create" className="navbar_link">
+          <Link to='/create' className='navbar_link'>
             Share
+          </Link>
+          <Link to='/create' className='navbar_link'>
+            Popular1
           </Link>
         </div>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/create' element={<Create />} />
       </Routes>
     </Router>
   );
